1edbbdd35863c6fd6177fe36bb4d13c35c5121f7
matiroiz1 <roizmatias10@gmail.com>
2024-09-21 22:19:26 -0300

    Add files via upload

---
+++ src/main/java/com/example/inicial1/entities/Autor.java
@@ -0,0 +1,28 @@
+package com.example.inicial1.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.envers.Audited;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@Table(name = "Autor")
+@Audited
+
+public class Autor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String biografia;
+}
